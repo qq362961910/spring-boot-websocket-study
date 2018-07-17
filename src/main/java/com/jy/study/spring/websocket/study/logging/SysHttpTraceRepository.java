9ba3a9c933175b1c2c45788469dea85228e8a20c
yangjian <yangjian>
2018-07-17 10:28:24 +0800

    project添加info

--- src/main/java/com/jy/study/spring/websocket/study/logging/SysHttpTraceRepository.java
+++ src/main/java/com/jy/study/spring/websocket/study/logging/SysHttpTraceRepository.java
@@ -3,23 +3,13 @@ package com.jy.study.spring.websocket.study.logging;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.trace.http.HttpTrace;
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public class SysHttpTraceRepository implements HttpTraceRepository {
+public class SysHttpTraceRepository extends InMemoryHttpTraceRepository {
 
     private static final Logger logger = LoggerFactory.getLogger(SysHttpTraceRepository.class);
-
-    /**
-     * 查询所有请求日志
-     * */
-    @Override
-    public List<HttpTrace> findAll() {
-        return null;
-    }
 
     /**
      * 存储请求日志
@@ -27,5 +17,6 @@ public class SysHttpTraceRepository implements HttpTraceRepository {
     @Override
     public void add(HttpTrace trace) {
         logger.info("http trace: uri: {}, time taken: {}", trace.getRequest().getUri(), trace.getTimeTaken());
+        super.add(trace);
     }
 }
