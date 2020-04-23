19f6bc2adbc0a4283e57f1e3021efccb79232901
yangjian <yangjian@ishansong.com>
2020-04-23 21:47:21 +0800

    整合traceId

--- src/main/java/com/jy/study/spring/websocket/study/controller/interceptor/WebSocketConnectionInterceptor.java
+++ src/main/java/com/jy/study/spring/websocket/study/controller/interceptor/WebSocketConnectionInterceptor.java
@@ -4,16 +4,22 @@ import com.jy.study.spring.websocket.study.config.properties.AppProperties;
 import com.jy.study.spring.websocket.study.entity.User;
 import com.jy.study.spring.websocket.study.service.UserRoleService;
 import com.jy.study.spring.websocket.study.service.UserTicketService;
+import org.slf4j.MDC;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.util.StringUtils;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 public class WebSocketConnectionInterceptor implements HandshakeInterceptor {
+
+    public static final String TRACE_ID_HEADER_NAME = "X-Request-Id";
+    public static final String TRACE_ID_LOG_NAME = "TRACE_ID";
 
     private UserTicketService userTicketService;
     private UserRoleService userRoleService;
@@ -21,6 +27,15 @@ public class WebSocketConnectionInterceptor implements HandshakeInterceptor {
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+        if(request instanceof HttpServletRequest) {
+            String traceId = ((HttpServletRequest)request).getHeader(TRACE_ID_HEADER_NAME);
+            if(StringUtils.isEmpty(traceId)) {
+                //todo generate traceId
+            } else {
+                MDC.put(TRACE_ID_LOG_NAME, traceId);
+            }
+        }
+
         if(request instanceof ServletServerHttpRequest) {
             Cookie[] cookies = ((ServletServerHttpRequest)request).getServletRequest().getCookies();
             if(cookies != null && cookies.length > 0) {
